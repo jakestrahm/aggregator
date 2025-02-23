e2cdfdb4c47ad677ae24d1644a174e28cf9f6b65
jake <jake@macbookpro.mynetworksettings.com>
2025-02-23 12:28:52 -0500

    make asynchandler less cryptic

--- server/src/middleware/asyncHandler.ts
+++ server/src/middleware/asyncHandler.ts
@@ -1,9 +1,18 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 
-const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) =>
-	//lambda
-	(req: Request, res: Response, next: NextFunction) => {
-		Promise.resolve(fn(req, res, next)).catch(next);
+// takes a request handler function as input
+const asyncHandler = (fn: RequestHandler) => {
+	// returns a new function that wraps the original handler
+	// this new function takes the standard express params
+	return (req: Request, res: Response, next: NextFunction) => {
+
+		// wraps the handler in a promise to ensure async handling
+		Promise.resolve(fn(req, res, next))
+			// if any errors occur, pass them to express error middleware
+			.catch(next);
 	}
+}
+
+
 
 export { asyncHandler }
