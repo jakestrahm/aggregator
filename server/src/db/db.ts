0b0768c25a875da508ac6347ceef08ec4643bd1d
jake <jake@mac.mynetworksettings.com>
2025-05-03 20:21:00 -0400

    clearing up console

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -4,9 +4,9 @@ import postgres, { Sql } from 'postgres'
 dotenv.config();
 
 export const sql: Sql = postgres(`${process.env.DATABASE_URL}`, {
-	debug: (connection, query, parameters) => {
-		console.log('connection: ', connection);
-		console.log('query: ', query);
-		console.log('parameters: ', parameters);
-	}
+	// debug: (connection, query, parameters) => {
+	// 	console.log('connection: ', connection);
+	// 	console.log('query: ', query);
+	// 	console.log('parameters: ', parameters);
+	// }
 });
